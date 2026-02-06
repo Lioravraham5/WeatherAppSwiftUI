bfb024b298e4caf6cb89753fb81284eef9492fef
Lior Avraham <Lior.Avraham@keshet-d.com>
2026-02-06 09:48:54 +0200

    Initial Commit

---
+++ WeatherAppSwiftUI/WeatherAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppSwiftUIApp.swift
+//  WeatherAppSwiftUI
+//
+//  Created by Lior.Avraham on 06/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
